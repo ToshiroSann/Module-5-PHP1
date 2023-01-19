7a1b0e8d720ef129f6cd573e180900ed450306f3
ToshiroSann <owenhomanlaw@gmail.com>
2023-01-19 14:49:52 +0100

    Commit

---
+++ extra/Challenge-1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="GET">
+    <div>
+        <input required id="kenteken" type="text" placeholder="kenteken" name="kenteken"><br>
+        <input type="text" placeholder="merk" name="merk"><br>
+        <input required type="text" placeholder="kleur" name="kleur"><br>
+        <input type="date" placeholder="bouwjaar" name="bouwjaar"><br>
+        <input type="text" placeholder="prijs" name="prijs">
+    </div>
+</form>
+</body>
+</html>
---
+++ extra/Challenge-1/sqlconn.php
@@ -0,0 +1,21 @@
+<?php
+$conn = mysqli_connect($servername, $username, $password);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "";
+}
+
+mysqli_close($conn);
+?>
