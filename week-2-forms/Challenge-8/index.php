3f131ebb8a980f29a7674a0dec8dfa4b9d42ee66
ToshiroSann <owenhomanlaw@gmail.com>
2023-01-24 11:02:17 +0100

    Done

--- week-2-forms/Challenge-8/index.php
+++ week-2-forms/Challenge-8/index.php
@@ -2,7 +2,7 @@
 
 echo "24h Clock";
 
-date_default_timezone_set("Netherlands/Amsterdam");
+date_default_timezone_set("Europe/Amsterdam");
 echo "<br><br>The time in Amsterdam: <br> " . date("H:i:s");
 
 date_default_timezone_set("America/New_York");
@@ -12,15 +12,16 @@ date_default_timezone_set("Australia/Sydney");
 echo "<br><br>The time in Sydney: <br> " . date("H:i:s");
 
 echo "<br><br>Date";
+echo "<br>" . date("l") . "<br>";
 
-date_default_timezone_set("Netherlands/Amsterdam");
+date_default_timezone_set("Europe/Amsterdam");
 
 echo "<br><br>Today is " . date("d/m/Y") . "<br>";
 echo "<br<br>Today is " . date("l");
 
 echo "<br><br>My birthday is in<br>";
 
-$birthday = "29th June";
+$birthday = "3th June";
 $days = round(abs(strtotime($birthday)-time()) / 86400);
 echo $days." days";
 
